f21a0205f4b6aaf7fecef723494972ebb22ab021
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2023-09-03 21:28:42 +0200

    Initial commit for the scaffold project.

---
+++ src/test/java/main.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class main {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    }
+}
