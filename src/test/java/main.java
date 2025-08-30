35ba0c5d4cd5188f3241ee699758daf51c96615b
Patryk <ostrowski.patryk@gmail.com>
2025-08-30 09:23:32 +0200

    Wyczyszczenie szablonu ze starych metod. Usunięcie zbędnych importów.

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,8 +1,5 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.assertj.core.api.Assertions;
 
 public class main {
     public static void main(String[] args) {
